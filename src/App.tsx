c71a1bdff927ec11745e9b38b9f1e41f8211b7a4
Emma Gerigscott <elgerig@gmail.com>
2023-12-31 12:14:08 -0800

    Refactor App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+const App: React.FC = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div className="App">
+      <main>
+        <div className="pink-circle"></div>
+        <div className="prompt">
+          <div className="prompt-title">Today's Prompt: </div>
+          <p className="prompt-body">Write a story about a person who is in a coma.</p>
+        </div>
+        <div className="timer">
+          <p>Time until next prompt:</p>
+          <p>00:00:00</p>
+        </div>
+      </main>
+      <footer>&copy; 2023 Emma Gerigscott - Ladies IT Department</footer>
+    </div>
+  );
+};
 
-export default App
+export default App;
