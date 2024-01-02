d6108bfdb48a555dc0fb7f71f4fe912e2c26bfb1
Emma Gerigscott <elgerig@gmail.com>
2024-01-01 16:44:36 -0800

    Add Prompt component to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
 import Timer from "./Timer";
+import Prompt from "./Prompt";
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <main>
-        <div className="pink-circle"></div>
-        <div className="prompt">
-          <div className="prompt-title">Today's Prompt: </div>
-          <p className="prompt-body">Write a story about a person who is in a coma.</p>
-        </div>
+        <Prompt />
         <Timer />
       </main>
       <footer>&copy; 2023 Emma Gerigscott - Ladies IT Department</footer>
