46b18fd7137fb0bfc768e418664066bb4a40625a
Emma Gerigscott <elgerig@gmail.com>
2024-01-03 13:07:08 -0800

    Refactor fetch response to return JSON data

--- src/services/promptAPI.ts
+++ src/services/promptAPI.ts
@@ -1,7 +1,7 @@
 const getPrompt = async () => {
   const response = await fetch(import.meta.env.VITE_PROMPT_URL);
-  const data = await response.text();
-  return data;
+  const data = await response.json();
+  return data.prompt;
 }
 
 export default getPrompt;
