47cdb407c80fdf8e10e7e9bfcea97c05660ff8ed
Emma Gerigscott <elgerig@gmail.com>
2024-01-03 13:07:19 -0800

    Refactor useState in Prompt component

--- src/Prompt.tsx
+++ src/Prompt.tsx
@@ -2,12 +2,11 @@ import getPrompt from "./services/promptAPI";
 import { useEffect, useState } from "react";
 
 const Prompt = () => {
-  const [prompt, setPrompt] = useState([]);
+  const [prompt, setPrompt] = useState('');
 
   const generatePrompt = async () => {
     const prompt = await getPrompt();
-    const promptArray = prompt.split(" ")
-    setPrompt(promptArray);
+    setPrompt(prompt);
   };
 
   useEffect(() => {
@@ -16,24 +15,9 @@ const Prompt = () => {
 
   return (
     <div className="prompt">
-      <div className="blue-circle"></div>
-      <svg viewBox="0 140 300 300" xmlns="http://www.w3.org/2000/svg">
-        <text x="50%" y="50%" textAnchor="middle" dy="3em" className="prompt-body">
-          {prompt.slice(0, 5).join(" ")}
-        </text>
-        <text x="50%" y="50%" textAnchor="middle" dy="5em" className="prompt-body">
-          {prompt.slice(5, 10).join(" ")}
-        </text>
-        <text x="50%" y="50%" textAnchor="middle" dy="7em" className="prompt-body">
-          {prompt.slice(10, 15).join(" ")}
-        </text>
-        <text x="50%" y="50%" textAnchor="middle" dy="9em" className="prompt-body">
-          {prompt.slice(15, 20).join(" ")}
-        </text>
-        <text x="50%" y="50%" textAnchor="middle" dy="11em" className="prompt-body">
-          {prompt.slice(20, prompt.length - 1).join(" ")}  
-        </text>
-        </svg>
+      <div className="blue-circle">
+        <div className="prompt-text">{prompt}</div>
+      </div>
     </div>
   );
 };
