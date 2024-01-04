0b6a649b6d7cc7fba4b5bd2306e083721fa71c2a
Emma Gerigscott <elgerig@gmail.com>
2024-01-03 16:39:49 -0800

    Delete getPrompt function

--- src/services/promptAPI.ts
+++
@@ -1,7 +0,0 @@
-const getPrompt = async () => {
-  const response = await fetch(import.meta.env.VITE_PROMPT_URL);
-  const data = await response.json();
-  return data.prompt;
-}
-
-export default getPrompt;
